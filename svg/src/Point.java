4e16367ced4f4141f582ab14dd05f23cc2e30805
ergaxyn <nikodem.wisniewski.nw@gmail.com>
2025-03-07 08:20:18 +0100

    test commit

--- svg/src/Point.java
+++ svg/src/Point.java
@@ -4,6 +4,15 @@ public class Point {
     public double x;
     public double y;
 
+    public Point() {
+
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public String toString()//zad1
     {
         return "Point{"
